0dfc6cbe092e648b1be7c28fd29567426c7f26c5
Ash <ash@ashryanbeats.com>
2016-03-11 15:55:20 -0600

    Added Application subclass.

---
+++ image-editor-ui/app/src/main/java/com/adobe/imageeditorui/MainApplication.java
@@ -0,0 +1,32 @@
+package com.adobe.imageeditorui;
+
+import android.app.Application;
+
+import com.adobe.creativesdk.foundation.AdobeCSDKFoundation;
+import com.adobe.creativesdk.foundation.auth.IAdobeAuthClientCredentials;
+
+/**
+ * Created by ash on 3/11/16.
+ */
+public class MainApplication extends Application implements IAdobeAuthClientCredentials {
+
+    /* Be sure to fill in the two strings below. */
+    private static final String CREATIVE_SDK_CLIENT_ID = "<YOUR_CLIENT_ID_HERE>";
+    private static final String CREATIVE_SDK_CLIENT_SECRET = "<YOUR_CLIENT_SECRET_HERE>";
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        AdobeCSDKFoundation.initializeCSDKFoundation(getApplicationContext());
+    }
+
+    @Override
+    public String getClientID() {
+        return CREATIVE_SDK_CLIENT_ID;
+    }
+
+    @Override
+    public String getClientSecret() {
+        return CREATIVE_SDK_CLIENT_SECRET;
+    }
+}
