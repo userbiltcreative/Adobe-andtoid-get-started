6bb0c1860ea6e3efe4048ec5e912f8fabb712d20
Ash <ash@ashryanbeats.com>
2016-03-11 16:17:50 -0600

    Launching Image Editor in onCreate().

--- image-editor-ui/app/src/main/java/com/adobe/imageeditorui/MainActivity.java
+++ image-editor-ui/app/src/main/java/com/adobe/imageeditorui/MainActivity.java
@@ -1,13 +1,17 @@
 package com.adobe.imageeditorui;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+
+import com.adobe.creativesdk.aviary.AdobeImageIntent;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,6 +21,17 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        /* 1) Make a new Uri object (Replace this with a real image on your device) */
+        Uri imageUri = Uri.parse("content://media/external/images/media/1248");
+
+        /* 2) Create a new Intent */
+        Intent imageEditorIntent = new AdobeImageIntent.Builder(this)
+                .setData(imageUri)
+                .build();
+
+        /* 3) Start the Image Editor with request code 1 */
+        startActivityForResult(imageEditorIntent, 1);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
