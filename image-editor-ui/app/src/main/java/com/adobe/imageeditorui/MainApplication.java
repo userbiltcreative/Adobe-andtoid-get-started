ef6c8eb0a4dcfd8a8b9eec457f51ba7a2576b485
Ash <ash@ashryanbeats.com>
2016-03-11 16:09:52 -0600

    Implemented IAviaryClientCredentials interface.

--- image-editor-ui/app/src/main/java/com/adobe/imageeditorui/MainApplication.java
+++ image-editor-ui/app/src/main/java/com/adobe/imageeditorui/MainApplication.java
@@ -2,13 +2,13 @@ package com.adobe.imageeditorui;
 
 import android.app.Application;
 
+import com.adobe.creativesdk.aviary.IAviaryClientCredentials;
 import com.adobe.creativesdk.foundation.AdobeCSDKFoundation;
-import com.adobe.creativesdk.foundation.auth.IAdobeAuthClientCredentials;
 
 /**
  * Created by ash on 3/11/16.
  */
-public class MainApplication extends Application implements IAdobeAuthClientCredentials {
+public class MainApplication extends Application implements IAviaryClientCredentials {
 
     /* Be sure to fill in the two strings below. */
     private static final String CREATIVE_SDK_CLIENT_ID = Keys.CSDK_CLIENT_ID;
@@ -28,5 +28,10 @@ public class MainApplication extends Application implements IAdobeAuthClientCred
     @Override
     public String getClientSecret() {
         return CREATIVE_SDK_CLIENT_SECRET;
+    }
+
+    @Override
+    public String getBillingKey() {
+        return ""; // Leave this blank
     }
 }
