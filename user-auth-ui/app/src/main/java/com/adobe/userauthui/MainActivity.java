1bce4a10fe2e613f40857743a32ada49867594a0
Ash <ash@ashryanbeats.com>
2016-03-12 12:55:02 -0600

    Added lifecycle methods for checking user auth status.

--- user-auth-ui/app/src/main/java/com/adobe/userauthui/MainActivity.java
+++ user-auth-ui/app/src/main/java/com/adobe/userauthui/MainActivity.java
@@ -1,5 +1,6 @@
 package com.adobe.userauthui;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -71,6 +72,42 @@ public class MainActivity extends AppCompatActivity {
         /* 5 */
         Log.i(MainActivity.class.getSimpleName(), "User is logged in!");
 
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mAuthSessionHelper.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mAuthSessionHelper.onPause();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mAuthSessionHelper.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mAuthSessionHelper.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mAuthSessionHelper.onDestroy();
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        mAuthSessionHelper.onActivityResult(requestCode, resultCode, data);
     }
 
     @Override
