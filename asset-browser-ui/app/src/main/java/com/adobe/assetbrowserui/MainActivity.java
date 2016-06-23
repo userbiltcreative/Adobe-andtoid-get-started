27b570e67d2f5c7cb4742065f5ac7cc14eda5c81
Ash <ash@ashryanbeats.com>
2016-06-23 18:49:19 -0400

    Removing fab.

--- asset-browser-ui/app/src/main/java/com/adobe/assetbrowserui/MainActivity.java
+++ asset-browser-ui/app/src/main/java/com/adobe/assetbrowserui/MainActivity.java
@@ -4,8 +4,6 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
@@ -62,15 +60,7 @@ public class MainActivity extends AppCompatActivity {
             }
         };
         mLaunchAssetBrowserButton.setOnClickListener(mLaunchAssetBrowserButtonListener);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        
     }
 
     private void launchAssetBrowser() {
