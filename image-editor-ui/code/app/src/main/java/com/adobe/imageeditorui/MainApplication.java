b79825d4f7e081fef69a3f126da09646d7ab9eb6
Ash <ash@ashryanbeats.com>
2016-11-09 15:44:53 -0500

    Updated code for new Image Editor release.

--- image-editor-ui/code/app/src/main/java/com/adobe/imageeditorui/MainApplication.java
+++ image-editor-ui/code/app/src/main/java/com/adobe/imageeditorui/MainApplication.java
@@ -2,17 +2,18 @@ package com.adobe.imageeditorui;
 
 import android.app.Application;
 
+import com.adobe.creativesdk.aviary.IAdobeAuthRedirectCredentials;
 import com.adobe.creativesdk.foundation.AdobeCSDKFoundation;
 import com.adobe.creativesdk.foundation.auth.IAdobeAuthClientCredentials;
 
 /**
  * Created by ash on 3/11/16.
  */
-public class MainApplication extends Application implements IAdobeAuthClientCredentials {
+public class MainApplication extends Application implements IAdobeAuthClientCredentials, IAdobeAuthRedirectCredentials {
 
-    /* Be sure to fill in the two strings below. */
     private static final String CREATIVE_SDK_CLIENT_ID = Keys.CSDK_CLIENT_ID;
     private static final String CREATIVE_SDK_CLIENT_SECRET = Keys.CSDK_CLIENT_SECRET;
+    private static final String CREATIVE_SDK_REDIRECT_URI = Keys.CSDK_REDIRECT_URI;
 
     @Override
     public void onCreate() {
@@ -30,4 +31,8 @@ public class MainApplication extends Application implements IAdobeAuthClientCred
         return CREATIVE_SDK_CLIENT_SECRET;
     }
 
+    @Override
+    public String getRedirectUri() {
+        return CREATIVE_SDK_REDIRECT_URI;
+    }
 }
